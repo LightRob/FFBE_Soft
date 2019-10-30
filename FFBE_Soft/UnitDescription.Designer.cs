d104cfef0c3c7e34164b861b823d7ac184bacd7a
YVARD Robin <robin.yvard@gmail.com>
2019-10-30 09:49:18 +0100

    UnitDesription Form update

--- FFBE_Soft/UnitDescription.Designer.cs
+++ FFBE_Soft/UnitDescription.Designer.cs
@@ -30,86 +30,94 @@
         {
             this.label_UnitName = new System.Windows.Forms.Label();
             this.pictureBox_UnitIdle = new System.Windows.Forms.PictureBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl_Unit = new System.Windows.Forms.TabControl();
+            this.tabPage_UnitStats = new System.Windows.Forms.TabPage();
+            this.listView_UnitStats = new System.Windows.Forms.ListView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.listView1 = new System.Windows.Forms.ListView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_UnitIdle)).BeginInit();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabControl_Unit.SuspendLayout();
+            this.tabPage_UnitStats.SuspendLayout();
             this.SuspendLayout();
             // 
             // label_UnitName
             // 
             this.label_UnitName.AutoSize = true;
             this.label_UnitName.Font = new System.Drawing.Font("Franklin Gothic Medium", 32F);
-            this.label_UnitName.Location = new System.Drawing.Point(12, 9);
+            this.label_UnitName.Location = new System.Drawing.Point(8, 6);
+            this.label_UnitName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_UnitName.Name = "label_UnitName";
-            this.label_UnitName.Size = new System.Drawing.Size(167, 81);
+            this.label_UnitName.Size = new System.Drawing.Size(110, 50);
             this.label_UnitName.TabIndex = 0;
             this.label_UnitName.Text = "Nom";
             // 
             // pictureBox_UnitIdle
             // 
-            this.pictureBox_UnitIdle.Location = new System.Drawing.Point(26, 116);
+            this.pictureBox_UnitIdle.Location = new System.Drawing.Point(17, 75);
+            this.pictureBox_UnitIdle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox_UnitIdle.Name = "pictureBox_UnitIdle";
-            this.pictureBox_UnitIdle.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox_UnitIdle.Size = new System.Drawing.Size(67, 32);
             this.pictureBox_UnitIdle.TabIndex = 1;
             this.pictureBox_UnitIdle.TabStop = false;
             // 
-            // tabControl1
+            // tabControl_Unit
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(384, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1502, 1000);
-            this.tabControl1.TabIndex = 2;
+            this.tabControl_Unit.Controls.Add(this.tabPage_UnitStats);
+            this.tabControl_Unit.Controls.Add(this.tabPage2);
+            this.tabControl_Unit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tabControl_Unit.Location = new System.Drawing.Point(122, 0);
+            this.tabControl_Unit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl_Unit.Name = "tabControl_Unit";
+            this.tabControl_Unit.SelectedIndex = 0;
+            this.tabControl_Unit.Size = new System.Drawing.Size(1143, 666);
+            this.tabControl_Unit.TabIndex = 2;
             // 
-            // tabPage1
+            // tabPage_UnitStats
             // 
-            this.tabPage1.Controls.Add(this.listView1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1494, 967);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage_UnitStats.Controls.Add(this.listView_UnitStats);
+            this.tabPage_UnitStats.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_UnitStats.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage_UnitStats.Name = "tabPage_UnitStats";
+            this.tabPage_UnitStats.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage_UnitStats.Size = new System.Drawing.Size(1135, 640);
+            this.tabPage_UnitStats.TabIndex = 0;
+            this.tabPage_UnitStats.Text = "Statistiques";
+            this.tabPage_UnitStats.UseVisualStyleBackColor = true;
+            // 
+            // listView_UnitStats
+            // 
+            this.listView_UnitStats.HideSelection = false;
+            this.listView_UnitStats.Location = new System.Drawing.Point(25, 26);
+            this.listView_UnitStats.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView_UnitStats.Name = "listView_UnitStats";
+            this.listView_UnitStats.Size = new System.Drawing.Size(491, 150);
+            this.listView_UnitStats.TabIndex = 0;
+            this.listView_UnitStats.UseCompatibleStateImageBehavior = false;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1494, 967);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1135, 640);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(38, 40);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(734, 229);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // UnitDescription
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1898, 1024);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(1265, 666);
+            this.Controls.Add(this.tabControl_Unit);
             this.Controls.Add(this.pictureBox_UnitIdle);
             this.Controls.Add(this.label_UnitName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UnitDescription";
             this.Text = "UnitDescription";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_UnitIdle)).EndInit();
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
+            this.tabControl_Unit.ResumeLayout(false);
+            this.tabPage_UnitStats.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,9 +127,9 @@
 
         private System.Windows.Forms.Label label_UnitName;
         private System.Windows.Forms.PictureBox pictureBox_UnitIdle;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl_Unit;
+        private System.Windows.Forms.TabPage tabPage_UnitStats;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView_UnitStats;
     }
 }
